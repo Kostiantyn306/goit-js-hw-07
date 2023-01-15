f4e7268183e60d785be4469ed15638a1a8fa6930
Kostiantyn306 <kostiantynmelnyk306@gmail.com>
2023-01-15 13:34:52 +0200

    №1

---
+++ js/02-lightbox.js
@@ -0,0 +1,17 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const allImg = document.querySelector('.gallery');
+const listImg = onGallery(galleryItems);
+allImg.insertAdjacentHTML("beforeend", listImg);
+
+function onGallery (galleryItems) {
+
+    return galleryItems.map(({description,original,preview}) => {
+return `<a data-lightbox="lbox"  class="gallery__item" href="${original}">
+<img onclick="return false"; class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join("") 
+}
+let lightbox = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+
+
